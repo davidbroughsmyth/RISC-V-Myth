2166071e88faabd57d5b9e762915e7fc7d2699a9
David Brough-Smyth <davidb@javahouse.com.au>
2025-08-08 16:57:23 +1000

    add riscv64 compile-disassembly-run

--- lab1/lab1.sh
+++ lab1/lab1.sh
@@ -1,2 +1,8 @@
 gcc -o sum1toN sum1toN.c
 ./sum1toN
+riscv64-unknown-elf-gcc -O1  -o rv64_sum1toN-O1.o sum1toN.c
+riscv64-unknown-elf-objdump -d  rv64_sum1toN-O1.o > rv64_sum1toN-O1_dis.txt
+riscv64-unknown-elf-gcc -Ofast  -o rv64_sum1toN-Ofast.o sum1toN.c
+riscv64-unknown-elf-objdump -d  rv64_sum1toN-Ofast.o > rv64_sum1toN-Ofast_dis.txt
+time spike pk rv64_sum1toN-O1.o
+time spike pk rv64_sum1toN-Ofast.o
